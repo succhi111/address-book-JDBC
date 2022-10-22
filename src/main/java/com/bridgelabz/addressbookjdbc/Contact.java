3cf5ea56358f3fac21601d6fbb151b1ff4254998
succhi111 <suchendramishra396@gmail.com>
2022-10-22 21:02:45 +0530

    Initial commit

---
+++ src/main/java/com/bridgelabz/addressbookjdbc/Contact.java
@@ -0,0 +1,115 @@
+package com.bridgelabz.addressbookjdbc;
+
+import java.time.LocalDate;
+
+public class Contact {
+    public String firstname;
+    public String lastname;
+    public String type;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public String number;
+    public String email;
+    public LocalDate date;
+    public String id;
+
+    // constructor
+    public Contact(String firstname, String lastname, String type, String address, String city, String state,String zip, String number, String email, LocalDate date) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.type = type;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.number = number;
+        this.email = email;
+        this.date = date;
+    }
+
+    public Contact(String firstname, String city) {
+        this.firstname = firstname;
+        this.city = city;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+        result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+        result = prime * result + ((number == null) ? 0 : number.hashCode());
+        result = prime * result + ((state == null) ? 0 : state.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        result = prime * result + ((zip == null) ? 0 : zip.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contact other = (Contact) obj;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        } else if (!city.equals(other.city))
+            return false;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (firstname == null) {
+            if (other.firstname != null)
+                return false;
+        } else if (!firstname.equals(other.firstname))
+            return false;
+        if (lastname == null) {
+            if (other.lastname != null)
+                return false;
+        } else if (!lastname.equals(other.lastname))
+            return false;
+        if (number == null) {
+            if (other.number != null)
+                return false;
+        } else if (!number.equals(other.number))
+            return false;
+        if (state == null) {
+            if (other.state != null)
+                return false;
+        } else if (!state.equals(other.state))
+            return false;
+        if (type == null) {
+            if (other.type != null)
+                return false;
+        } else if (!type.equals(other.type))
+            return false;
+        if (zip == null) {
+            if (other.zip != null)
+                return false;
+        } else if (!zip.equals(other.zip))
+            return false;
+        return true;
+    }
+}
